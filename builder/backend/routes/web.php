5748e59f9f8af6d51131dbac6c932ecd5fd15117
AlexTuring010 <alexgkiafis@gmail.com>
2026-07-02 12:08:29 +0300

    chore: add Laravel backend scaffold and first routes

---
+++ builder/backend/routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/test-text', function () {
+    return "<p>This is a test paragraph.</p>";
+});
+
+Route::get('/test-php-array', function () {
+    $testArray = ['name' => 'John Doe', 'age' => 30, 'email' => 'john.doe@example.com'];
+    return $testArray;
+});
+
